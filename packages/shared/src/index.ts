38f59378d41700cd013f775847f822e46f7df49f
Gowtham S <gowtham54321@yahoo.com>
2026-05-30 02:15:23 +0100

    feat: re-export shared types at top level for namespace-free imports

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,3 +3,9 @@ export * from './types/index.js'
 export * as content from './content/index.js'
 export * as eigenai from './eigenai/index.js'
 export * as viem from './viem/index.js'
+
+// Convenience type re-exports for consumers that don't want the namespace prefix.
+export type { PackedFramework, PinResult, PinataConfig } from './content/index.js'
+export type { AssembledPrompt } from './eigenai/prompt.js'
+export type { EigenAIConfig, InferenceResult } from './eigenai/client.js'
+export type { ChainClients, SupportedNetwork } from './viem/clients.js'
